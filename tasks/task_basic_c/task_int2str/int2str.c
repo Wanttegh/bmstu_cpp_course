8da338a2b64bc72cf3ee25f078c6bb095f9a6edd
howard <wantte@yandex.ru>
2024-11-02 12:00:19 +0300

    Версия от 02 ноября 2024 года

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -2,31 +2,35 @@
 #include "int2str.h"
 #include "stdio.h"
 #include <string.h>
+#include <stdlib.h>
 
 char * int2str(int number) {
-    if (number == 0) return "0";
-    else if (number == -2147483648) return "-2147483648"; 
-    else if (number == 2147483647) return "2147483647";
-    else {
-        char str [12];
-        int i = 0;
-        int sign = (number >= 0 ? 1 : -1);
-        number *= sign;
-        while (number > 0){
-            str[i++] = number % 10 + '0';
-            number /= 10;
-        }
-        if (sign < 0) str[i++] = '-';
-        str[i] = '\0';
-        for (int j = 0, k = i - 1; j < k; j++, k--){
-            char c = str[j];
-            str[j] = str[k];
-            str[k] = c;
-        }
-        char * const res = (char *)malloc(i*sizeof(char *));
-        for (int m = 0; m <= i; m++){
-            res[m] = str[m];
-        }
-        return res;
+    int i = 2, int_l = 0;
+    int sign = (number >= 0 ? 1 : -1);
+    int number_copy = number;
+    while (number_copy > 10 || number_copy < -10){
+        i++; int_l++;
+        number_copy /= 10;
     }
+    i++; int_l++;
+    if (sign < 0) i++;
+    char * str = calloc(i, 1);
+    int it = 0, check = 0;
+    while (check != int_l){
+        str[it++] = (sign*(number % 10) + '0');
+        check++;
+        number /= 10;
+    }
+    if (sign < 0) str[it++] = '-';
+    str[it] = '\0';
+    for (int j = 0, k = it - 1; j < k; j++, k--){
+        char c = str[j];
+        str[j] = str[k];
+        str[k] = c;
+    }
+    char * res = calloc(i, 1);
+    for (int m = 0; m <= i; m++){
+        res[m] = str[m];
+    }
+    return res;
 }
